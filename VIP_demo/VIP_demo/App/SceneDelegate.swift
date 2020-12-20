7d3f6a5de74669d61db877b10d056fec7fd496b1
Borja Saez de Guinoa Vilaplana <borja.saez@v2msoft.com>
2020-12-20 11:54:21 +0100

    UPDATED sceneDelegate to instantiate first viewcontroller programatically. Removed references to main storyboard.

--- VIP_demo/VIP_demo/App/SceneDelegate.swift
+++ VIP_demo/VIP_demo/App/SceneDelegate.swift
@@ -13,10 +13,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        setupRootViewController(scene: scene)
         guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+    func setupRootViewController(scene: UIScene) {
+        if let windowScene:UIWindowScene = scene as? UIWindowScene{
+            self.window = UIWindow(windowScene: windowScene)
+            let testVC = UIViewController()
+            testVC.view.backgroundColor = .red
+            self.window?.rootViewController = testVC
+            self.window?.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
